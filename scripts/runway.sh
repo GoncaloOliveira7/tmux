4f0d56ef113c55a54d4d0a663224752016df6f1e
Gonçalo Oliveira <goncalo@TUIL04HL6L913.local>
2024-11-27 16:43:12 +0000

    initial commit

---
+++ scripts/runway.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SESH="runway"
+
+tmux has-session -t $SESH 2>/dev/null
+
+if [ $? != 0 ]; then
+	tmux new-session -d -s $SESH -n "editor"
+
+	tmux send-keys -t $SESH:editor "cd ~/work_dir/runway/" C-m
+	tmux send-keys -t $SESH:editor "nvim ." C-m
+	tmux split-window -h
+
+	tmux new-window -t $SESH -n "server"
+	tmux send-keys -t $SESH:server "cd ~/work_dir/runway/" C-m
+	tmux send-keys -t $SESH:server "yarn install && runway_dev" C-m
+	tmux split-window -h
+	tmux send-keys -t $SESH:server "cd ~/work_dir/runway/" C-m
+
+	tmux select-window -t $SESH:editor
+	tmux select-pane -t 1
+	tmux resize-pane -Z
+fi
+
+tmux attach-session -t $SESH
+
